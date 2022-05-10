263e25120bfd2e8adb109c6df739327ad5d34ffc
LuongTDT <tamluong.td@gmail.com>
2022-05-11 02:12:46 +0700

    adding table summary template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel - Louis has just modified!
                 </div>
 
                 <div class="links">
