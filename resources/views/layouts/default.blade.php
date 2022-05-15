449e31d406700c6d2be4e28cab5fa3836e39cd8d
LuongTDT <tamluong.td@gmail.com>
2022-05-15 10:33:36 +0700

    simple css for general files

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,24 +1,67 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Tournament management</title>
-
+    <link rel="stylesheet" href="{{url('css/summary-table.css')}}">
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
     <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+        integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
     <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous">
+    </script>
+        <style type="text/css">
+        .round {
+        float: left;
+        width: 160px;
+        }
+
+        .firstTeam,
+        .secondTeam {
+        border-bottom: 1px solid #ccc;
+        position: relative;
+        }
+
+        .firstSpacer,
+        .secondTeam {
+        border-right: 1px solid #ccc;
+        }
+
+        .team {
+        position: absolute;
+        bottom: 1px;
+        left: 1px;
+        }
+    </style>
 
 </head>
-<body>
+
+<body style ="background-color:#EEEEEE;">
     <div class="container">
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="{{URL::to('/')}}">ournament management</a>
+                </div>
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="{{URL::to('summary')}}">Summary</a></li>
+                    <li><a href="{{ route('athletes.index') }}">Athletes</a></li>
+                    <li><a href="{{ route('tournaments.index') }}">Tournaments</a></li>
+                    <li><a href="{{ route('forms.index') }}">Forms</a></li>
+                </ul>
+            </div>
+        </nav>
         @yield('content')
     </div>
 </body>
+
 </html>
--- resources/views/summary.blade.php
+++ resources/views/summary.blade.php
@@ -1,36 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Summary</title>
-    <link rel="stylesheet" href="{{url('css/summary-table.css')}}">
-    <style>
-
-    </style>
-</head>
-
-<body>
-    <h1><span class="blue">&lt;</span>Summary of<span class="blue">&gt;</span> <span class="yellow">MATCHES</pan>
-    </h1>
-    <h2>Created with love by LuongTDT</h2>
+@extends('layouts.default')
+@section('content')
+    <h1 style = "padding-top: 5%;font-weight: bold;"><span class="blue">&lt;</span>Summary of MATCHES<span class="blue">&gt;</span> <span class="yellow">MATCHES</pan></h1>
+    <h4 style = "">result of matches for today: {{date('d-m-y')}}</h4>
 
     <table class="container">
         <thead>
             <tr>
                 <th>
-                    <h1>Sites</h1>
+                    <h1>No</h1>
                 </th>
                 <th>
-                    <h1>Views</h1>
+                    <h1>Form name</h1>
                 </th>
                 <th>
-                    <h1>Clicks</h1>
+                    <h1>first Athlete</h1>
                 </th>
                 <th>
-                    <h1>Average</h1>
+                    <h1>second Athlete</h1>
+                </th>
+                <th>
+                    <h1>Result 1</h1>
+                </th>
+                <th>
+                    <h1>Result 2</h1>
+                </th>
+                <th>
+                    <h1>Result 3</h1>
+                </th>
+                <th>
+                    <h1>Plan Start</h1>
+                </th>
+                <th>
+                    <h1>Actual</h1>
                 </th>
             </tr>
         </thead>
@@ -40,10 +41,20 @@
                 <td>9518</td>
                 <td>6369</td>
                 <td>01:32:50</td>
+                <td>7326</td>
+                <td>10437</td>
+                <td>00:51:22</td>
+                <td>10437</td>
+                <td>00:51:22</td>
             </tr>
             <tr>
                 <td>Twitter</td>
                 <td>7326</td>
+                <td>10437</td>
+                <td>00:51:22</td>
+                <td>7326</td>
+                <td>10437</td>
+                <td>00:51:22</td>
                 <td>10437</td>
                 <td>00:51:22</td>
             </tr>
@@ -52,27 +63,23 @@
                 <td>4162</td>
                 <td>5327</td>
                 <td>00:24:34</td>
+                <td>7326</td>
+                <td>10437</td>
+                <td>00:51:22</td>
+                <td>10437</td>
+                <td>00:51:22</td>
             </tr>
             <tr>
                 <td>LinkedIn</td>
                 <td>3654</td>
                 <td>2961</td>
                 <td>00:12:10</td>
-            </tr>
-            <tr>
-                <td>CodePen</td>
-                <td>2002</td>
-                <td>4135</td>
-                <td>00:46:19</td>
-            </tr>
-            <tr>
-                <td>GitHub</td>
-                <td>4623</td>
-                <td>3486</td>
-                <td>00:31:52</td>
+                <td>7326</td>
+                <td>10437</td>
+                <td>00:51:22</td>
+                <td>10437</td>
+                <td>00:51:22</td>
             </tr>
         </tbody>
     </table>
-</body>
-
-</html>
+@endsection
